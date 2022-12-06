724b0659fa34555d9545d68ba5df3e24120e4151
Qusay <qusay@nadsoft.net>
2022-12-06 21:18:20 +0300

    webpack config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,22 +2,11 @@ import type { StorybookConfig } from "@storybook/core-common";
 
 const config: StorybookConfig = {
 	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-	staticDirs: ["../public"],
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/addon-interactions",
 	],
-	typescript: {
-		check: false,
-		checkOptions: {},
-		reactDocgen: "react-docgen-typescript",
-		reactDocgenTypescriptOptions: {
-			shouldExtractLiteralValuesFromEnum: true,
-			propFilter: (prop) =>
-				prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-		},
-	},
 	framework: "@storybook/react",
 	core: {
 		builder: "webpack5",
--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,13 @@
+import { devServer } from "@cypress/webpack-dev-server";
 import { defineConfig } from "cypress";
-import { devServer } from "@cypress/vite-dev-server";
-import viteConfig from "./vite.config";
+
 export default defineConfig({
 	component: {
 		devServer(devServerConfig) {
 			return devServer({
 				...devServerConfig,
 				framework: "react",
-				viteConfig,
+				webpackConfig: require("./cypress/webpack.config"),
 			});
 		},
 	},
