16cfd81f671078015a318599e4ddc44cedd9420a
Qusay <qusay@nadsoft.net>
2022-12-18 14:57:32 +0300

    story book separate

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/core-common";
 
 const config: StorybookConfig = {
-	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+	stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
