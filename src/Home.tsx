c0129aeabc923b148fb4238d3bc139922ecc6b8e
Deyaa Mutlaq <deyaa@nadsoft.net>
2022-12-26 10:44:47 +0300

    clear input package from side bar modal

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,42 +3,42 @@ import { useState } from "react";
 import SideBar from "@packages/core-sidebar";
 
 export default function Home() {
-  const [openSideBar, setOpenSideBar] = useState(false);
+	const [openSideBar, setOpenSideBar] = useState(false);
 
-  const sideBarStyle = {
-    bmCrossButton: {
-      height: "24px",
-      width: "24px",
-    },
-    bmCross: {
-      background: "red",
-    },
+	const sideBarStyle = {
+		bmCrossButton: {
+			height: "24px",
+			width: "24px",
+		},
+		bmCross: {
+			background: "red",
+		},
 
-    bmMenu: {
-      background: "black",
-      padding: "2.5em 1.5em 0",
-      fontSize: "1.15em",
-    },
-    bmOverlay: {
-      background: "rgba(0, 0, 0, 0.3)",
-    },
-  };
+		bmMenu: {
+			background: "black",
+			padding: "2.5em 1.5em 0",
+			fontSize: "1.15em",
+		},
+		bmOverlay: {
+			background: "rgba(0, 0, 0, 0.3)",
+		},
+	};
 
-  return (
-    <>
-      <div>
-        <SideBar
-          styles={sideBarStyle}
-          right={true}
-          customBurgerIcon={false}
-          isOpen={openSideBar}
-          setIsOpen={setOpenSideBar}
-        >
-          <p style={{ margin: 0 }}>test</p>
-        </SideBar>
-        <button onClick={() => setOpenSideBar(true)}>open side bar</button>
-        <CoolButton label="Code" />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<SideBar
+					styles={sideBarStyle}
+					right={true}
+					customBurgerIcon={false}
+					isOpen={openSideBar}
+					setIsOpen={setOpenSideBar}
+				>
+					<p style={{ margin: 0 }}>test</p>
+				</SideBar>
+				<button onClick={() => setOpenSideBar(true)}>open side bar</button>
+				<CoolButton label="Code" />
+			</div>
+		</>
+	);
 }
