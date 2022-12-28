b0a28c5cfa5ff76b34d64971d0517ac927153c0f
Qusay <qusay@nadsoft.net>
2022-12-28 15:18:20 +0300

    fix sort

--- src/packages/Table/logic/sort.ts
+++ src/packages/Table/logic/sort.ts
@@ -1,15 +1,13 @@
-const sort = (source: Array<any>, sourceId: string, targetId: string) => {
-	const oldSourceIndex = source.findIndex((item) => item.id === sourceId);
-	const oldTargetIndex = source.findIndex((item) => item.id === targetId);
-	const nextData = source.filter((item) => item.id !== sourceId);
-	const dragItem = source.find((item) => item.id === sourceId);
-	let index = nextData.findIndex((item) => item.id === targetId);
-
-	if (oldTargetIndex > oldSourceIndex) {
-		index = index + oldTargetIndex - oldSourceIndex;
-	}
-	nextData.splice(index, 0, dragItem);
-	return nextData;
+import { arrayMoveImmutable } from "array-move";
+const sort = (
+	source: Array<any>,
+	sourceId: string | number,
+	targetId: string,
+) => {
+	const oldSourceIndex = source.findIndex((item) => item.id == sourceId);
+	const oldTargetIndex = source.findIndex((item) => item.id == targetId);
+	const res = arrayMoveImmutable(source, oldSourceIndex, oldTargetIndex);
+	return res;
 };
 
 export { sort };
