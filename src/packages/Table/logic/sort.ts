5a5a8f8a96fe3d449432323391b4c88346307d4f
Qusay <qusay@nadsoft.net>
2022-12-28 15:18:43 +0300

    fix sort

--- src/packages/Table/logic/sort.ts
+++ src/packages/Table/logic/sort.ts
@@ -1,9 +1,5 @@
 import { arrayMoveImmutable } from "array-move";
-const sort = (
-	source: Array<any>,
-	sourceId: string | number,
-	targetId: string,
-) => {
+const sort = (source: Array<any>, sourceId: string, targetId: string) => {
 	const oldSourceIndex = source.findIndex((item) => item.id == sourceId);
 	const oldTargetIndex = source.findIndex((item) => item.id == targetId);
 	const res = arrayMoveImmutable(source, oldSourceIndex, oldTargetIndex);
